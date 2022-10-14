f0acbc251a66d85c908104bfa99b813d5448e537
neeckola <mladi.filozof@gmail.com>
2022-10-14 02:51:27 +0200

    ep4

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
                 'purple': '#8b60ed',
             },
             maxWidth: {
-                custom: '62.5rem',
+                custom: '68.5rem',
             },
             spacing: {
                 70: '17.5rem',
