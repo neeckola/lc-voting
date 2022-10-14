3e0e785ee2df184202477ef52660c9e8dadede00
neeckola <mladi.filozof@gmail.com>
2022-10-14 19:51:16 +0200

    ep12

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -51,7 +51,7 @@
             </div>
 
             <div class="flex flex-1 px-2 py-6">
-                <div class="flex-none"><a href="#"><img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl"></a></div>
+                <div class="flex-none"><a href="#"><img src="{{ $idea->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl"></a></div>
                 <div class="w-full mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="{{ route('idea.show', $idea) }}" class="idea-link hover:underline">{{ $idea->title }}</a>
